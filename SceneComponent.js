1eeb2931ae3eddf5ae425af4d5d81b5a17d5e3ed
Mo Binni <mo.binni@rangle.io>
2017-03-03 09:57:41 -0500

    Update SceneComponent.js

--- SceneComponent.js
+++ SceneComponent.js
@@ -3,8 +3,6 @@ const ReactNative = require('react-native');
 const {Component, } = React;
 const {View, StyleSheet, } = ReactNative;
 
-const StaticContainer = require('react-native/Libraries/Components/StaticContainer');
-
 const SceneComponent = (Props) => {
   const {shouldUpdated, ...props, } = Props;
   return <View {...props}>
